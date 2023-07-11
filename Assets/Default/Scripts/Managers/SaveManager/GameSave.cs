c6ad2a852da901f4608b17f950ae6fc098180f81
arinsanghib <72905071+arinsanghib@users.noreply.github.com>
2023-07-11 23:54:50 +0530

    Initial commit

---
+++ Assets/Default/Scripts/Managers/SaveManager/GameSave.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CompleteProject
+{
+    [System.Serializable]
+    public class GameSave
+    {
+        public GameData GameData;
+        public PlayerData PlayerData;
+        public List<EnemyData> EnemiesData;
+    }
+}
